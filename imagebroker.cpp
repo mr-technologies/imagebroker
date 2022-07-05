fb5d2937fbec33dd7a36b02a54a09bbb73e6171d
parafin <parafin@paraf.in>
2022-07-05 17:08:42 +0200

    [*] render images from CUDA buffer using OpenGL

--- imagebroker.cpp
+++ imagebroker.cpp
@@ -1,3 +1,5 @@
+#define OPENCV_HAS_CUDA_AND_OPENGL 1
+
 // std
 #include <map>
 #include <stack>
@@ -14,10 +16,17 @@ using json = nlohmann::json;
 
 // OpenCV
 #include <opencv2/opencv.hpp>
+#if OPENCV_HAS_CUDA_AND_OPENGL
+#include <opencv2/core/opengl.hpp>
+#endif
 #include <opencv2/highgui.hpp>
 
 // IFF SDK
 #include "iff.h"
+
+
+const int MAX_WINDOW_WIDTH  = 1280;
+const int MAX_WINDOW_HEIGHT = 1024;
 
 struct exporter_t
 {
@@ -62,63 +71,80 @@ int main()
 
     auto current_chain = chains[0];
 
+#if OPENCV_HAS_CUDA_AND_OPENGL
+    using Mat = cv::cuda::GpuMat;
+#else
+    using Mat = cv::Mat;
+#endif
     std::mutex render_mutex;
-    cv::Mat render_image;
+    Mat render_image;
 
-    auto export_func = new exporter_t();
-    auto cb = [&](const void* data, size_t size, iff_image_metadata* metadata)
+    exporter_t export_func;
+    export_func.invoke = [&](const void* data, size_t size, iff_image_metadata* metadata)
     {
-        cv::Mat dst_image;
-        dst_image.create(cv::Size(metadata->width, metadata->height), CV_8UC4);
         void* img_data = const_cast<void*>(data);
-        cv::Mat src_image(cv::Size(metadata->width, metadata->height), CV_8UC4, img_data, metadata->width * 4 + metadata->padding);
-        src_image.copyTo(dst_image);
-
+        Mat src_image(cv::Size(metadata->width, metadata->height), CV_8UC4, img_data, metadata->width * 4 + metadata->padding);
         std::scoped_lock<std::mutex> render_lock(render_mutex);
-        render_image = dst_image;
+        src_image.copyTo(render_image);
     };
-    export_func->invoke = std::move(cb);
-
     iff_set_export_callback(current_chain, "exporter",
                             [](const void* data, size_t size, iff_image_metadata* metadata, void* private_data)
                             {
                                 auto export_function = (exporter_t*)private_data;
                                 export_function->invoke(data, size, metadata);
                             },
-                            export_func);
-
+                            &export_func);
 
     iff_execute(current_chain, json{ {"exporter", {{"command", "on"}}} }.dump().c_str());
 
     std::string window_name = "IFF SDK Image Broker Sample";
+    bool size_set = false;
+#if OPENCV_HAS_CUDA_AND_OPENGL
+    cv::namedWindow(window_name, cv::WINDOW_NORMAL | cv::WINDOW_OPENGL);
+    cv::setWindowProperty(window_name, cv::WND_PROP_VSYNC, 1);
+    cv::ogl::Texture2D tex;
+#else
     cv::namedWindow(window_name, cv::WINDOW_NORMAL);
-    cv::resizeWindow(window_name, 1024, 1024);
-
+#endif
 
     printf("To terminate program press `Esc` key\n");
-    while (true)
+    while(true)
     {
-        std::unique_lock<std::mutex> render_lock(render_mutex);
-        auto image_to_show = render_image;
-        render_lock.unlock();
-
-        if(image_to_show.empty())
-        {
-            image_to_show = cv::Mat(2048, 2048, CV_8UC4, cv::Scalar(0, 0, 0, 255));
-        }
-
-        cv::imshow(window_name, image_to_show);
-        if(cv::waitKey(5) == 27)
+        if(cv::pollKey() == 27)
         {
             printf("Esc key is pressed by user. Stopping application.\n");
             break;
+        }
+        std::scoped_lock<std::mutex> render_lock(render_mutex);
+        if(!render_image.empty())
+        {
+            if(!size_set)
+            {
+                auto size = render_image.size();
+                if(size.width > MAX_WINDOW_WIDTH)
+                {
+                    size.height = MAX_WINDOW_WIDTH / size.aspectRatio();
+                    size.width = MAX_WINDOW_WIDTH;
+                }
+                if(size.height > MAX_WINDOW_HEIGHT)
+                {
+                    size.width = MAX_WINDOW_HEIGHT * size.aspectRatio();
+                    size.height = MAX_WINDOW_HEIGHT;
+                }
+                cv::resizeWindow(window_name, size);
+                size_set = true;
+            }
+#if OPENCV_HAS_CUDA_AND_OPENGL
+            tex.copyFrom(render_image);
+            cv::imshow(window_name, tex);
+#else
+            cv::imshow(window_name, render_image);
+#endif
         }
     }
 
     iff_release_chain(current_chain);
     iff_finalize();
-
-    delete export_func;
 
     return 0;
 }
